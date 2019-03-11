b5a126f8c4079467f03eeb66af35fd2af979b5bc
Nikita1999UA <boludenikita@gmail.com>
2019-03-11 17:45:46 +0200

    create page

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.technologies--slider').slick({
+        dots: true,
+        slidesToShow: 6,
+        centerMode: true,
+        centerPadding: '-10px',
+        prevArrow: '<img class="slick__prev" src="../img/left.png">',
+        nextArrow: '<img class="slick__next" src="../img/right.png   ">',
+    });
+  });
+
+  
